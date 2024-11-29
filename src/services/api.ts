dc70bcd1c57b8f6a3b7273e78c2282bc2b03e650
Yasser Carreon <yacasia@gmail.com>
2024-11-29 12:19:58 -0600

    Add API service for fetching tax brackets by year

---
+++ src/services/api.ts
@@ -0,0 +1,28 @@
+const API_ENDPOINT = "http://localhost:5001/tax-calculator/tax-year/";
+
+export interface TaxBracket {
+  min: number;
+  max: number;
+  rate: number;
+}
+
+/**
+ * Fetches the tax brackets for a given year from the API.
+ *
+ * @param {string} year - The year for which to fetch the tax brackets.
+ * @returns {Promise<TaxBracket[]>} A promise that resolves to an array of tax brackets.
+ * @throws {Error} If the response from the API is not ok.
+ */
+export const fetchTrackBracketsApi = async (
+  year: string
+): Promise<TaxBracket[]> => {
+  const response = await fetch(`${API_ENDPOINT}${year}`);
+
+  if (!response.ok) {
+    throw new Error(`Error: ${response.statusText}`);
+  }
+
+  const { tax_brackets } = await response.json();
+
+  return tax_brackets;
+};
