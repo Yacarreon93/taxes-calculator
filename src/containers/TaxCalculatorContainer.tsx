eb13a96cf7e14bb5eb5c8b9421302cc885d0f703
Yasser Carreon <yacasia@gmail.com>
2024-11-29 12:24:34 -0600

    Refactor TaxCalculatorContainer to use custom hook for fetching tax brackets and streamline state management

--- src/containers/TaxCalculatorContainer.tsx
+++ src/containers/TaxCalculatorContainer.tsx
@@ -1,37 +1,24 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Box, Typography, Container } from "@mui/material";
 import TaxCalculatorForm from "../components/molecules/TaxCalculatorForm";
 import TaxCalculatorResults from "../components/molecules/TaxCalculatorResults";
+import useResults from "../hooks/useResults";
+import { fetchTrackBracketsApi } from "../services/api";
+
+interface TaxBracket {
+  min: number;
+  max: number;
+  rate: number;
+}
 
 const TaxCalculatorContainer = () => {
   const [year, setYear] = useState("2022");
   const [salary, setSalary] = useState("");
-  const [error, setError] = useState<Error | null>(null);
-  const [loading, setLoading] = useState(false);
-  const [results, setResults] = useState([]);
 
-  const fetchTaxData = async () => {
-    setLoading(true);
-    setError(null);
-
-    try {
-      const response = await fetch(
-        `http://localhost:5001/tax-calculator/tax-year/${year}`
-      );
-
-      if (!response.ok) {
-        throw new Error(`Error: ${response.statusText}`);
-      }
-
-      const result = await response.json();
-
-      setResults(result);
-    } catch (err) {
-      setError(err as Error);
-    }
-
-    setLoading(false);
-  };
+  const { loading, error, results, fetchResults } = useResults(
+    () => fetchTrackBracketsApi(year),
+    (rawData: TaxBracket[]) => rawData
+  );
 
   const handleChange = (key: string, value: string) => {
     if (key === "year") {
@@ -42,8 +29,6 @@ const TaxCalculatorContainer = () => {
     }
   };
 
-  const handleSumit = () => fetchTaxData();
-
   return (
     <Container maxWidth="sm">
       <Box display="flex" flexDirection="column" gap={4}>
@@ -52,7 +37,7 @@ const TaxCalculatorContainer = () => {
           year={year}
           salary={salary}
           onChange={handleChange}
-          onSubmit={handleSumit}
+          onSubmit={fetchResults}
         />
         <TaxCalculatorResults
           error={error}
