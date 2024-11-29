a17d37153510360f3ee98be47caea6e8fd9ee6eb
Yasser Carreon <yacasia@gmail.com>
2024-11-28 20:44:57 -0600

    Add title to Tax Calculator container

--- src/containers/TaxCalculatorContainer.tsx
+++ src/containers/TaxCalculatorContainer.tsx
@@ -1,11 +1,13 @@
 import Container from "@mui/material/Container";
 import TaxCalculatorForm from "../components/molecules/TaxCalculatorForm";
+import { Typography } from "@mui/material";
 
 const TaxCalculatorContainer = () => {
   const handleSumit = () => {};
 
   return (
     <Container maxWidth="sm">
+      <Typography variant="h1">Tax Calculator</Typography>
       <TaxCalculatorForm onSubmit={handleSumit} />
     </Container>
   );
